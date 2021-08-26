bf46706873b8a29974372694d92c6de5a03ace50
Justin Perez <perezjrp936@gmail.com>
2021-08-26 15:27:21 -0400

    added questions for new intern/engineer

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
-const promptUser = () => {
+const promptManager = () => {
     console.log(`
     =================
     Welcome To Team Manager! Please Start By Entering Manager Role Information.
@@ -66,10 +66,74 @@ const promptUser = () => {
     ])  
 };
 
-promptUser()
+const promptEmployee = () =>{
+    console.log(`
+    =================
+    Add a New Employee
+    =================
+    `);
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the employee`s name? (Required)',
+                validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter their name!');
+                    return false;
+                }
+                }
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the employee`s ID? (Required)',
+                validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter their id!');
+                    return false;
+                }
+                }
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the employee`s email? (Required)',
+                validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter their email!');
+                    return false;
+                }
+                }
+            },
+            {
+                type: 'list',
+                name: 'employeetype',
+                message: 'What kind of employee is this? (Required)',
+                choices:['Engineer', 'Intern']
+            }
+        ])
+        .then(({employeetype}) =>{
+            if(employeetype === 'Engineer'){
+                console.log('This is an Engineer');
+            }
+            if(employeetype === 'Intern'){
+                 console.log('This is an Intern');
+            }
+        });
+};
+
+promptManager()
     .then((data)=>{ 
         teamManager = new Manager(new Employee(data.name,data.id,data.email),data.office);
-        
+        promptEmployee();
     })
     .catch(err =>{
         console.log(err);
